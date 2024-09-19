dfbc3c153675654c48ea9d3f8ea6b0676066d69c
Kdoby <155566596+Kdoby@users.noreply.github.com>
2024-09-19 14:44:35 +0900

    Title: 최대 증가 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240919/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int N;
 int ans;
 vector<int> vec;
-int dp[1001];
+int dp[1000];
 
 int main() {
     cin>>N;
@@ -14,24 +14,17 @@ int main() {
         cin>>num;
         vec.push_back(num);
     }
-    for(int i=1;i<=N;++i) {
+    for(int i=0;i<N;++i) {
         dp[i] = 1;
     }
     for(int i=0; i<N; ++i) {
-        int tmp = vec[i];
-        int len = 1;
         for(int j=i+1; j<N; ++j) {
-            if(tmp < vec[j]) {
-                ++len;
-                tmp = vec[j];
-                dp[j] = max(dp[j], len);
-            }
-            else {
-                continue;
-            }
+           if(vec[i] < vec[j]) {
+                dp[j] = max(dp[j], dp[i] + 1);
+           }
         }
     }
-    for(int i=1; i<=N; ++i) {
+    for(int i=0; i<N; ++i) {
         ans = max(ans, dp[i]);
     }
     cout<<ans;
