4b0ab06ff2854b404af7ee04cf3bfaeacc9d5e57
Kdoby <155566596+Kdoby@users.noreply.github.com>
2026-03-18 23:49:51 +0900

    Title: 기울어진 직사각형, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 260318/기울어진 직사각형/slanted-rectangle.java
+++ 260318/기울어진 직사각형/slanted-rectangle.java
@@ -1,53 +1,58 @@
 import java.util.Scanner;
 public class Main {
+    public static int[][] grid = new int[20][20];
+    public static int n;
 
-    private static int[] xpos = {-1, 1, 0, 0};
-    private static int[] ypos = {0, 0, -1, 1};
-    private static int ans = 0;
+    public static int ans = 0;
 
-    public static int GetSum(int[][] grid, int row, int col, int n) {
+    public static boolean inRange(int row, int col) {
+        return 0 <= row && row < n && 0 <= col && col < n;
+    }
+
+    // k = 가로 길이, l = 세로 길이
+    public static int solve(int row, int col, int k, int l) {
+        // 1 -> 2 -> 3 -> 4 를 각 변의 길이만큼씩 순회
+        int[] dx = new int[]{-1, -1, 1, 1};
+        int[] dy = new int[]{1, -1, -1, 1};
+        int[] moveNum = new int[]{k, l, k, l};
+
         int sum = 0;
+        int xx = row;
+        int yy = col;
+
         for(int i = 0; i < 4; ++i) {
-            int x = row + xpos[i];
-            int y = col + ypos[i];
-            if(x < 0 || x >= n || y < 0 || y >= n) return 0;
-            sum += grid[x][y];
+            // moveNum = k or l
+            for(int j = 0; j < moveNum[i]; ++j) {
+                xx += dx[i]; yy += dy[i];
+
+                // 범위 벗어나는 사각형은 탈락
+                if(!inRange(xx, yy)) return 0;
+
+                sum += grid[xx][yy];
+            }
         }
 
         return sum;
     }
-
-    public static int GetLeftDiagonalSum(int[][] grid, int row, int col, int n) {
-        if(row + 1 >= n || col - 1 < 0) return 0;
-        return grid[row + 1][col] + grid[row][col - 1];
-    }
-
-    public static int GetRightDiagonalSum(int[][] grid, int row, int col, int n) {
-        if(row + 1 >= n || col + 1 >= n ) return 0;
-        return grid[row + 1][col] + grid[row][col + 1];
-    }
+    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] grid = new int[n][n];
+        n = sc.nextInt();
+        
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
         // Please write your code here.
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++){
-                int sum = GetSum(grid, i, j, n);
-                if(sum == 0) continue;
-                int lsum = sum;
-                int rsum = sum;
-                for(int k = 1; k < n; ++k) {
-                    if(i + k < n && j - k >= 0)
-                        lsum += GetLeftDiagonalSum(grid, i + k, j - k, n);
-                    if(i + k < n && j + k < n) 
-                        rsum += GetRightDiagonalSum(grid, i + k, j + k, n);
+                for(int k = 1; k < n; k++) {
+                    for(int l = 1; l < n; l++) {
+                        // 각 점의 위치가 (row, col) 사각형의 맨 밑이라고 가정
+                        // 이 점에서부터 1 -> 2 -> 3 -> 4 순서로 순회
+                        ans = Math.max(ans, solve(i, j, k, l));
+                    }
                 }
-                ans = Math.max(Math.max(ans, lsum), rsum);
             }
         }
 
